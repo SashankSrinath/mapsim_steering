3756eeb8dc8b8e580eac046b73c8da183c3e8afe
Sashank Srinath <105050513+SashankSrinath@users.noreply.github.com>
2022-05-22 15:38:50 +0200

    Add files via upload
    Updating launch file to simulate the map and removed the lasers (temporarily, in line 26 )
    Updated the fwd_kinematics node with subscriber data being recorded in a global variable. Tried adding the kinematics. v_x and w_z but its not working as of now.
    Simulation still runs with jsp = True

--- launch/simulation_launch.py
+++ launch/simulation_launch.py
@@ -9,11 +9,21 @@ def generate_launch_description():
     sl.declare_arg('cmd', default_value=True)
     sl.declare_arg('jsp', default_value=True)
     
-    sl.node('map_simulator', 'simulator', parameters = {'map': sl.find('mapsim_steering', 'void.yaml'),
-                                                        'display': False})
+    sl.declare_arg('map', default_value=sl.find('map_simulator', 'house.yaml'))
+    sl.declare_arg('max_height', default_value=800)
+    sl.declare_arg('max_width', default_value=1200)
+    sl.declare_arg('rate', default_value=20)
+    sl.declare_arg('map_server', default_value=False)
+    
+    node_params = {}
+    node_params['map'] = sl.arg('map')
+    node_params['max_height'] = sl.arg('max_height')
+    node_params['max_width'] = sl.arg('max_width')
+    node_params['rate'] = sl.arg('rate')
+    sl.node('map_simulator', 'simulator', parameters = node_params, output='screen')
 	
     with sl.group(ns=sl.arg('robot')):  
-        sl.node('map_simulator', 'spawn', parameters = {'static_tf_odom': True})
+        sl.node('map_simulator', 'spawn', parameters = {'static_tf_odom': True, 'force_scanner' : False})
         sl.include('mapsim_steering', 'steering_launch.py', launch_arguments=sl.arg_map(('robot', 'cmd', 'jsp')))
         
     sl.node('rviz2', 'rviz2', arguments=['-d', sl.find('mapsim_steering', 'config.rviz')],
--- src/fwd_kinematics.cpp
+++ src/fwd_kinematics.cpp
@@ -7,6 +7,7 @@
 #include <memory>
 #include <geometry_msgs/msg/twist.hpp>
 #include <std_msgs/msg/float32_multi_array.hpp>
+#include <std_msgs/msg/float32.hpp>
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -15,6 +16,11 @@ using std::placeholders::_1;
 // Subscribes to the 'cmd' topic (float32MultiArray) published by the slider_publisher
 
 //To do - The kinematic model for the 1,1 robot
+// joint state publisher from the slider publisher commands
+
+
+std_msgs::msg::Float32MultiArray::SharedPtr get_msg;
+float v_x, w_z;
 
 class fwd_kinematics_node : public rclcpp::Node
 {
@@ -22,45 +28,68 @@ public:
     fwd_kinematics_node()
     : Node("fwd_kinematics_node")
     {
-      publisher_  = this->create_publisher<geometry_msgs::msg::Twist>("/bike/cmd_vel", 1);
+        subscriber_ = create_subscription<std_msgs::msg::Float32MultiArray>("cmd", 1,
+                    std::bind(&fwd_kinematics_node::topic_callback, this, _1));
 
-      subscriber_ = create_subscription<std_msgs::msg::Float32MultiArray>("cmd", 1,
-                  std::bind(&fwd_kinematics_node::topic_callback, this, _1));
+        publisher_  = this->create_publisher<geometry_msgs::msg::Twist>("/bike/cmd_vel", 1);
 
-      timer_ = create_wall_timer(
-      50ms, std::bind(&fwd_kinematics_node::timer_callback, this));
+        timer_ = create_wall_timer(10ms, std::bind(&fwd_kinematics_node::timer_callback, this));
     }
 
 private:
     void timer_callback()
     {
+
+      //RCLCPP_INFO(this->get_logger(), "timer_callback"); // just to test
+
       auto message = geometry_msgs::msg::Twist();
-      message.linear.x = 10.0;
+      message.linear.x =1.0;
       message.linear.y = 0.0;
       message.linear.z = 0.0;
       message.angular.x = 0.0;
       message.angular.y = 0.0;
-      message.angular.z = 1.0;
+      message.angular.z = 0.1;
+
+      //message = velocity_measure();
+      //v_x = message.linear.x;
+      //w_z = message.angular.z;
+
+     // RCLCPP_INFO(this->get_logger(), "message.linear.x: '%f'", v_x);
+      //RCLCPP_INFO(this->get_logger(), "message.angular.z: '%f'", w_z);
 
       RCLCPP_INFO_ONCE(this->get_logger(), "\n\nPublishing: '%f'\n\n", message.linear.x);
+      //RCLCPP_INFO_ONCE(this->get_logger(), "\n\n wz: '%f'\n\n", message.angular.z);
+
       publisher_->publish(message);
+
     }
 
     void topic_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg) const
-        {
-          RCLCPP_INFO(this->get_logger(), "v_control '%f'", msg->data[0]);
-          RCLCPP_INFO(this->get_logger(), "beta_dot_control '%f'", msg->data[1]);
-        }
+    {
+        get_msg = msg;
+       // RCLCPP_INFO(this->get_logger(), "v_control: '%f'", get_msg->data[0]);
+       // RCLCPP_INFO(this->get_logger(), "beta_dot_control: '%f'", get_msg->data[1]);
+    }
 
+    geometry_msgs::msg::Twist velocity_measure()
+    {
+        auto message = geometry_msgs::msg::Twist();
+        static float v = get_msg->data[0];
+        static float beta = 0.0;
+        static float dt = 0.01; // 100 hz or 10ms
+        beta += dt*get_msg->data[1];
+        message.linear.x = v*cos(beta);
+        message.linear.y = 0.0;
+        message.linear.z = 0.0;
+        message.angular.x = 0.0;
+        message.angular.y = 0.0;
+        message.angular.z = v*sin(beta)/0.95; // L = 0.95
+        return message;
+    }
+
+    rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr subscriber_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-    //size_t count_;
-
-    //subscriber
-    std_msgs::msg::Float32MultiArray sub_cmd;
-    rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr subscriber_;
-    public:
-
 };
 
 int main (int argc, char** argv)
@@ -70,3 +99,16 @@ int main (int argc, char** argv)
   rclcpp::shutdown();
   return 0;
 }
+
+
+
+
+/*
+    void topic_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg) const
+        {
+          fwd_kinematics_node::v_control = msg->data[0];
+          fwd_kinematics_node::beta_dot_control = msg->data[1];
+          RCLCPP_INFO(this->get_logger(), "v_control: '%f'", fwd_kinematics_node::v_control);
+          RCLCPP_INFO(this->get_logger(), "beta_dot_control: '%f'", fwd_kinematics_node::beta_dot_control);
+        }
+*/
